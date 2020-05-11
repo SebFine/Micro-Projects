0709b7699a0da160e20e8d8e5545f268ad3c6429
62475734 <62475734+SebFine@users.noreply.github.com>
2020-05-11 10:26:02 +0200

    update 11.05.20

--- Dice/dice.js
+++ Dice/dice.js
@@ -1,30 +1,51 @@
-const displayResult = document.querySelector("#result");
+const displayResultSum = document.querySelector("#result-sum");
+const displayResultArr = document.querySelector("#result-arr");
 const diceChoice = document.querySelector("#dice");
 const rollButton = document.querySelector("#roll");
-
-// Create a random number based on diceChoice
-function random(num) {
-    let result = Math.floor(Math.random()*num)+1;
-    return result;
-}
+const throwsBox = document.querySelector("#throws");
 
 // getting the values from dice choice
 function getValues() {
-    let num = Number(diceChoice.value);
-    return num;
-}
+    sides = Number(diceChoice.value);
+    return sides;
+};
+
+function getThrows() {
+    throws = Number(throwsBox.value); // froms always return strings
+    return throws;
+};
+
+// Create a random number based on diceChoice
+function randomSide(sides, throws) {
+    const arr = [];
+    for (i = 1; i <= throws; i++) {
+        random = Math.floor(Math.random() * sides) + 1;
+        arr.push(random);
+    }
+    return arr;
+};
+
+// find out the sum of the array items
+function add(arr) {
+    for (const el of arr) {
+        sum += arr[i];
+    }
+    return sum;
+};
 
 // roll Button
 function roll() {
-    random(num);
-    displayResult.textContent = result;
-    console.log("button works")
-}
+    getValues();
+    getThrows();
+    const arr = randomSide(sides, throws);
+    const sum = add(arr);
+    displayResultSum.textContent = sum;
+    displayResultSum.textContent = arr.join(", ");
+};
 
 // event listener for roll Button and dice choice.
 rollButton.addEventListener("click", roll);
-diceChoice.addEventListener("change", getValues);
 
-// Anzahl der würfe
 
-// Ausgabe des ERgebnisses als summe und als Einzelwürfe
+
+
